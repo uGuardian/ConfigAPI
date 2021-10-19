0cb0a5f50a750cfafd5ae0670074efb6941bb3b8
uGuardian <Sean.Heavysege@gmail.com>
2021-10-19 09:20:05 -0600

    Updated Template

--- Template.cs
+++ Template.cs
@@ -1,17 +1,48 @@
 using System;
 
-// Your namespace here
+// REQUIRED: Rename the namespace to your own
 namespace Template {
 	public class ConfigInitializer : ModInitializer {
+		// REQUIRED: You need to either call the included method InitConfig or ConfigAPI.Init in a ModInitializer
+		// The included method InitConfig stops your modinitializer from crashing or throwing an error if ConfigAPI isn't loaded
 		public override void OnInitializeMod() {
-			// Put the name of your config file, and then the instance of the config class
-			ConfigAPI.Init("Template", TemplateConfig.Instance);
+			// REQUIRED: Put the name of your config file, and then the instance of the config class
+			this.InitConfig("Template", TemplateConfig.Instance);
+		}
+
+
+		// Checks to see if ConfigAPI is enabled, and then inits the config.
+		internal void InitConfig(string name, object config) {
+			List<String> assembly = new List<String>();
+			foreach (var a in AppDomain.CurrentDomain.GetAssemblies()) {
+				assembly.Add(a.GetName().Name);
+			}
+			if (assembly.Contains("ConfigAPI")) {
+				var tempInstance = new ConfigHandler();
+				tempInstance.Init(name, config);
+			}
+		}
+		// The actual init code is called in a separate class by InitConfig to prevent errors.
+		internal class ConfigHandler {
+			internal void Init(string name, object config) {
+				// If you directly call this yourself, put the name of your config file, and then the instance of the config class
+				ConfigAPI.Init(name, config);
+			}
 		}
 	}
 
 	[Serializable]
+	// REQUIRED: Change the class name
 	public class TemplateConfig {
-		// Change TemplateConfig to whatever the name of your config class is
+		// REQUIRED: Add your variables and their default values here, and call them via the instance
+		// Only public, non-static variables are officially supported
+		// Officially supported types are Bool, String, and most standard number types like Int
+		// Examples below:
+		public bool myBool = false;
+		public int myInt = 0;
+		public string myString = "myString";
+
+		// REQUIRED: Change both TemplateConfig to whatever the name of your class is
 		public static TemplateConfig Instance = new TemplateConfig();
 	}
 }
